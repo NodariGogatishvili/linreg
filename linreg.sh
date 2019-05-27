81fb5195c513949b2398ba68fd6b0aebe14d89ef
nodari <nodari@localhost.localdomain>
2019-05-27 14:30:03 +0200

    v0.6

--- linreg.sh
+++ linreg.sh
@@ -86,12 +86,17 @@ transpon(){
 			else
 				row="$tmp"
 			fi
+			#echo "$row" >&2
 		done
+		#echo "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+		#echo "$row" >&2
+		# problem
 		if [ -n "$M" ]; then
 			M="$M\n$row"
 		else
 			M="$row"
 		fi
+		echo "$M" >&2
 		row=""
 	done
 	echo "$M"
@@ -119,7 +124,7 @@ row(){
 	row=""
 }
 
-multiplyMatcies(){
+multiplyMatrices(){
 	n=`echo "$1" | wc -l`
 	m=`echo "$1" | head -1 | wc -w`
 	M2=`transpon "$2"`
@@ -177,10 +182,11 @@ gaussJordan(){
 	echo "$gauss" |
 	while read -r aLine; do
 		tmp=`echo "$aLine" | cut -f$i -d' '`
-		if [ $tmp -ne 0 ]; then
-			scalarXvector $((1/$tmp)) "$aLine"
+		if [ "$tmp" != "0" ]; then
+			tmp=`echo "scale=20; 1/$tmp" | bc`
+			scalarXvector $tmp "$aLine"
 		else
-			scalarCvector 0 "$aLine"
+			scalarXvector 0 "$aLine"
 		fi
 		i=$(($i+1))
 	done
@@ -188,7 +194,7 @@ gaussJordan(){
 	i=`echo "$1" | head -1 | wc -w`
 	echo "$gauss" |
 	while read -r aLine; do
-		echo "$gauss" | head -$((i-1)) |
+		echo "$gauss" | head -$(($i-1)) |
 		while read -r bLine; do
 			substractByPivot "$aLine" "$bLine" $i
 		done
@@ -231,7 +237,7 @@ regresion(){
 	AT=`transpon "$1"`
 	ATA=`multiplyMatrices "$AT" "$1"`
 	ATA1=`invert "$ATA"`
-	ATA1AT=`multiplyMatricies "$ATA1" "$AT"`
+	ATA1AT=`multiplyMatrices "$ATA1" "$AT"`
 	ATA1ATb=`multiplyMatrices "$ATA1AT" "$2"`
 	echo "$ATA1ATb"
 }
@@ -245,7 +251,7 @@ exponets(){
 }
 
 
-ColumnExponention(){
+columnExponention(){
 	echo "$1" |
         while read -r eLine; do
                 i=`echo "$eLine" | wc -w`
@@ -256,9 +262,9 @@ ColumnExponention(){
 
 matrixForLinReg(){
 	cat "$1" | tail -n +2 | cut -f1 -d' ' > tmpA
-	MfLR=`paste tmpA tmpA`
+	MfLR=`paste tmpA tmpA -d' '`
 	columnExponention "$MfLR"
-	rm tmpA tmpB
+	rm tmpA
 }
 
 drawLine(){
@@ -269,47 +275,46 @@ drawLine(){
 	firstY=`multiplyMatrices "$xT" "$minX 1"`
 	secondY=`multiplyMatrices "$xT" "$maxX 1"`
 	
-	firstY=$((1000-$firstY/$maxY*1000))
-	secondY=$(((-1)*$secondY/$maxY*1000))
+	firstY=`echo "scale=20; 1000-$firstY/$maxY*1000" | bc`
+	secondY=`echo "scale=20; (-1)*$secondY/$maxY*1000" | bc`
 	echo "
 	<path d=\"m 100 $firstY 1000 $secondY \" style=\"fill:none;stroke-width:1px;stroke:#000\"/>
 	" >> "$out"
 }
 
 drawSVG(){
-	echo '
-<svg width="1200" height="1200" version="1.1" viewBox="0 0 1200 1200" xmlns="http://www.w3.org/2000/svg">
+	echo '<svg width="1200" height="1200" version="1.1" viewBox="0 0 1200 1200" xmlns="http://www.w3.org/2000/svg">
   <!--background-->
   <rect x="0" y="0" width="1200" height="1200" style="fill:#fff"/>
   <rect x="100" y="100" width="1000" height="1000" style="fill:#fff"/>
-   
-  <!--axis x-->
-  <path d="m 100 1100 1000 0" style="fill:none;stroke-width:2px;stroke:#000"/>
-  <text x="1100" y="1150" style="fill:black;font-size:25px;font-family:sans-serif">x</text>
-  <path d="m1090 1090 10 10 -10 10 " style="fill:none;stroke-width:2px;stroke:black" />
+  '
+	echo "  <!--axis x-->
+  <path d=\"m 100 1100 1000 0\" style=\"fill:none;stroke-width:2px;stroke:#000\"/>
+  <text x=\"1140\" y=\"1110\" style=\"fill:black;font-size:25px;font-family:sans-serif\">$axisX</text>
+  <path d=\"m1090 1090 10 10 -10 10 \" style=\"fill:none;stroke-width:2px;stroke:black\" />
 
   <!--axis y-->
-  <path d="m 100 1100 0 -1000" style="fill:none;stroke-width:2px;stroke:#000"/>
-  <text x="50" y="100" style="fill:black;font-size:25px;font-family:sans-serif">y</text>
-  <path d="m90 110 10 -10 10 10 " style="fill:none;stroke-width:2px;stroke:black" />
-	' > "$out"
+  <path d=\"m 100 1100 0 -1000\" style=\"fill:none;stroke-width:2px;stroke:#000\"/>
+  <text x=\"90\" y=\"50\" style=\"fill:black;font-size:25px;font-family:sans-serif\">$axisY</text>
+  <path d=\"m90 110 10 -10 10 10 \" style=\"fill:none;stroke-width:2px;stroke:black\" />"
 }
 
 axisNumbering(){
 	echo "  <!-- axis numbering x in order-->"
-	for i in (seq 1 10); do
+	for i in $(seq 1 10); do
+	tmp=`echo "scale=2; $i*$maxX/10" | bc`
 	echo "
 	<path d=\"m $(($i*100+100)) 1090 0 20\" style=\"fill:none;stroke-width:2px;stroke:#000\"/>
-	<path d=\"m $(($i*100+100))  100 0 1000\" style=\"fill:none;stroke-width:1px;stroke:#000\"/>
-	<text x=\"$(($i*100+80))\" y=\"1150\"  style=\"fill:black;font-size:25px;font-family:sans-serif\">300</text>
-"
+	<path d=\"m $(($i*100+100)) 100 0 1000\" style=\"fill:none;stroke-width:1px;stroke:#000\"/>
+	<text x=\"$(($i*100+80))\" y=\"1150\"  style=\"fill:black;font-size:25px;font-family:sans-serif\">$tmp</text>"
 	done
 	echo "  <!-- axis numbering y in order-->"
-	for i in (seq 1 10); do
+	for i in $(seq 1 10); do
+	tmp=`echo "scale=2; $i*$maxY/10" | bc`
 	echo "
 	<path d=\" 90 $((1100-$i*100)) 10 0\" style=\"fill:none;stroke-width:2px;stroke:#000\"/>
-	<path d=\"m 90 $((1100-$i*100)) 1000 0\" style=\"fill:none;stroke-width:1px;stroke:#000\"/>
-	<text x=\"20\" y=\"$((1100-$i*100))\"  style=\"fill:black;font-size:25px;font-family:sans-serif\">300</text>
+	<path d=\"m 100 $((1100-$i*100)) 1000 0\" style=\"fill:none;stroke-width:1px;stroke:#000\"/>
+	<text x=\"20\" y=\"$((1110-$i*100))\"  style=\"fill:black;font-size:25px;font-family:sans-serif\">$tmp</text>
 "
 	done
 }
@@ -320,7 +325,11 @@ placeDots(){
 	while read -r line; do
 		tmpX=`echo "$line" | cut -f1 -d' '`
 		tmpY=`echo "$line" | cut -f2 -d' '`
-echo "<rect transform=\"translate($((100+$tmpX/$maxX*1000)) $((1000-$tmpY/$maxY*1000))) rotate(45)\" width=\"10\" height=\"10\" style=\"paint-order:normal\"/>"
+
+		tmpX=`echo "scale=20; 100+$tmpX/$maxX*1000" | bc`
+		tmpY=`echo "scale=20; 1100-$tmpY/$maxY*1000" | bc`
+		echo "
+		<rect transform=\"translate($tmpX $tmpY) rotate(45)\" width=\"10\" height=\"10\" style=\"paint-order:normal\"/>"
 	done
 }
 
@@ -328,10 +337,10 @@ minmax < $1
 
 out="out.svg"
 
-drawSVG
-axisNumbering
-placeDots < $1
-drawLine "$1"
+drawSVG > "$out"
+axisNumbering >> "$out"
+placeDots < $1 >> "$out"
+drawLine "$1" >> "$out"
 echo "</svg>" >> "$out"
 
 exit
